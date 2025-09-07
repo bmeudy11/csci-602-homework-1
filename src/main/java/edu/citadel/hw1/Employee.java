51fa8dee840e2a3ba5a134f513345b6de92cd9cf
bmeudy11 <bmeudy@gmail.com>
2025-09-07 12:40:18 -0400

    add base employee class

---
+++ src/main/java/edu/citadel/hw1/Employee.java
@@ -0,0 +1,10 @@
+package edu.citadel.hw1;
+
+/**
+ * An abstract class representing an employee, it implements
+ * Comparable<Employee></Employee>.
+ */
+public abstract class Employee implements Comparable<Employee> {
+
+
+}
