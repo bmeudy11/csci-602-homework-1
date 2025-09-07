bf12104a825d0501679b2f14711c4bcab7eef590
bmeudy11 <bmeudy@gmail.com>
2025-09-07 12:56:54 -0400

    initial buildout of HourlyEmployee class

--- src/main/java/edu/citadel/hw1/HourlyEmployee.java
+++ src/main/java/edu/citadel/hw1/HourlyEmployee.java
@@ -1,4 +1,21 @@
 package edu.citadel.hw1;
 
+import java.time.LocalDate;
+
+
+//represents employee who is paid hourly
 public class HourlyEmployee {
+    private double wageRate;
+    private double hoursWorked;
+
+    /**
+     * Public constructor that initializes all four fields (two
+     * declared in this class and two inherited from class `Employee`)
+     */
+    public HourlyEmployee(String name, LocalDate hireDate, double wageRate, double hoursWorked)
+    {
+
+    }
+
+
 }
