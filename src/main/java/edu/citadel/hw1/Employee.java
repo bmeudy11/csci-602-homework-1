ca0053e93abe6d65fed8a8d8f36777009aea2e80
bmeudy11 <bmeudy@gmail.com>
2025-09-07 12:45:06 -0400

    build out Employee class

--- src/main/java/edu/citadel/hw1/Employee.java
+++ src/main/java/edu/citadel/hw1/Employee.java
@@ -1,10 +1,32 @@
 package edu.citadel.hw1;
 
+import java.time.LocalDate;
+
 /**
  * An abstract class representing an employee, it implements
  * Comparable<Employee></Employee>.
  */
 public abstract class Employee implements Comparable<Employee> {
+    private String name;
+    private LocalDate hireDate;
+
+    //constructor initializing name and hireDate
+    public Employee(String name, LocalDate hireDate) {
+        this.name = name;
+        this.hireDate = hireDate;
+    }
+
+    //getter for name
+    //use lombok here?
+    public String getName() {
+        return name;
+    }
+
+    //getter for hireDate
+    //use lombok here?
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
 
 
 }
